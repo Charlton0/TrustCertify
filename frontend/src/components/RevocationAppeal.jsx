1d9a3dc2fd46e8e73670fd6ff7fea2504bea7170
charlton0 <cjuma0909@gmail.com>
2025-02-27 00:29:42 +0300

    Appeal revocation updated

--- frontend/src/components/RevocationAppeal.jsx
+++ frontend/src/components/RevocationAppeal.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import styles from "./RevocationAppeal.module.css"
 import Navigation from './Navigation';
+import { contract, web3, checkConnection } from "../utils/web3Utils";
+
 const RevocationAppeal = () => {
+
+   const [certificateID, setCertificateID] = useState("");
+   const [appealReason, setAppealResason] = useState("");
+   const [accounts, setAccounts] = useState(null); 
+
+     const checkConnection = async () => {
+           if (window.ethereum) {
+             try {
+               const accounts = await web3.eth.getAccounts();
+               if (accounts.length > 0) {
+                 setAccounts(accounts[0]);
+       
+                 return accounts[0];
+               } else {
+                 console.log("no accounts were found");
+                 return null;
+               }
+             } catch (e) {
+               console.error("error connecting to wallet " + e);
+               return null;
+             }
+           } else {
+             console.log("you need to install your metamask");
+           }
+         };
+       
+         useEffect(() => {
+           checkConnection();
+         }, []);
+     
+         const appealRevocation = async () => {
+           console.log({certificateID,appealReason});
+     
+             if (window.ethereum) {
+                 if (accounts) {
+                   const gas = await contract.methods
+                     .appealRevocation(certificateID)
+                     .estimateGas({ from: accounts });
+                   const tx = await contract.methods
+                     .appealRevocation(certificateID)
+                     .send({ from: accounts, gas });
+                   console.log(tx);
+                 }
+               }
+         };
   return (
     <>
     <Navigation />
@@ -11,21 +58,45 @@ const RevocationAppeal = () => {
             
             <div className={styles.form_section}>
                 <label for="certificateId">Certificate ID</label>
-                <input type="text" id="certificateId" name="certificateId" required/>
+                <input 
+                type="text" 
+                id="certificateId"
+                 name="certificateId"
+                 onChange={(e) => {
+                    setCertificateID(e.target.value);
+                  }}
+                  placeholder="Enter certificateId"
+                  required
+                  />
             </div>
             
             <div className={styles.form_section}>
                 <label for="appealReason">Reason for Appeal</label>
-                <textarea id="appealReason" name="appealReason" required></textarea>
+                <textarea
+                 id="appealReason"
+                  name="appealReason"
+                  onChange={(e) => {
+                    setappealReason(e.target.value);
+                  }}
+                  placeholder="Enter appealReason"
+                   required
+                   >
+
+                   </textarea>
             </div>
             
-            <div className={styles.upload_section}>
+            {/* <div className={styles.upload_section}>
                 <label for="supportingDocuments">Upload Supporting Documents (optional)</label>
                 <input type="file" id="supportingDocuments" name="supportingDocuments" accept=".pdf,.jpg,.png,.docx"/>
-            </div>
+            </div> */}
             
             <div className={styles.btn}>
-                <button type="submit">Submit Appeal</button>
+                <button 
+                type="button"
+                onClick={appealRevocation}
+                >
+                    Submit Appeal
+                    </button>
             </div>
         </form>
     </div> 
